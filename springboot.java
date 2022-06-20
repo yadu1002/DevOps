591ddc127ade85c2ee255f06cb2e1ea4acdcd582
yadu1002 <mandala.uday@gmail.com>
2022-06-20 19:14:18 +0000

    userfour added some code

--- springboot.java
+++ springboot.java
@@ -13,3 +13,21 @@ public class Factorial
 		return result;
 	}
 }
+//comment added by userfour and some java code added
+public class CallingMethodsInSameClass
+{
+	public static void main(String[] args) {
+		printOne();
+		printOne();
+		printTwo();
+	}
+
+	public static void printOne() {
+		System.out.println("Hello World");
+	}
+
+	public static void printTwo() {
+		printOne();
+		printOne();
+	}
+}
